950245930b9013cd9dd573ca9efd82070434f033
Kayla <kaylabenedetti10@gmail.com>
2019-04-22 14:49:12 -0700

    updated flask

--- Flask HW.py
+++ Flask HW.py
@@ -5,24 +5,20 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 import datetime as dt
 import pandas as pd
-
 from flask import Flask, jsonify
 
+app = Flask(__name__)
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
-# reflect an existing database into a new model
-Base = automap_base()
 # reflect the tables
-
+Base = automap_base()
+# reflect an existing database into a new model
 Base.prepare(engine, reflect=True)
-Base.classes.keys()
 
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
 session = Session(engine)
-
-app = Flask(__name__)
 
 @app.route("/")
 def home():
@@ -30,33 +26,108 @@ def home():
         f"Avaliable Routes:<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/tobs")
+        f"/api/v1.0/tobs>"
+        f"/api/v1.0/<start>"
+        f"/api/v1.0/<start>/<end>"
+        )
 
 #Return Convert the query results to a Dictionary using `date` as the key and `prcp` as the value.
 # Return the JSON representation of your dictionary.
+@app.route("/api/v1.0/precipitation")
 def precipitation():
-    results = session.query(Measurement.prcp, Measurement.date).all()
 
-    all_results = []
-    for prcp in results:
-        results_dict = {}
-        station_dict['Measurement.station'] = station
-        precipitation_dict['Measurement.prcp'] = precipitation
-        dat_dict['Measurement.date'] = date
-        all_results.append(results_dict)
+    start_date = "2017-4-1"
+    s_date = dt.datetime.strptime(start_date, '%Y-%m-%d')
 
-    return jsonify(all_results)
+    previous_date = start_date - relativedelta(years=1)
+
+    results = session.query(Measurement.prcp, Measurement.date).\
+        filter(Measurement.date > previous_date).all()
+
+    last_date=list()
+    last_prcp=list()
+    count=0
+    for x in results:
+        count+1
+        last_date.append(x.date)
+        last_prcp.append(x.prcp)
+        if count % 200 == 0 :
+            print(x.date, x.prcp)
+
+    prcp_dict={"Date": last_date, "Precipitation": last_prcp}
+
+    return jsonify(prcp_dict)
         
 
-#Return a JSON list of stations from the dataset.
-def station():
-    return jsonify(Measurement.station)
+@app.route("/api/v1.0/stations")
+def stations():
+    stations = session.query(Measurement.station).distinct().all()
+    return jsonify(stations)
 
-# Query for the dates and temperature observations from a year from the last data point.
-# Return a JSON list of Temperature Observations (tobs) for the previous year.
+@app.route("/api/v1.0/tobs")
 def tobs():
-    last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
-    last_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    station_temps = session.query(Measurement.tobs).\
+        filter(Measurement.date > previous_date).all()
+    return jsonify(station_temps)
+    
+
+@app.route("/api/v1.0/<start>/<end>")
+def tempst(start,end):
+
+    st_date_inp  = start
+    end_date_inp = end
+    
+    try:
+        st_date_conv =  dt.datetime.strptime(st_date_inp, '%Y-%m-%d')
+        
+    except:
+        print(" Invalid start date or date format. Please input valid date in yyyy-mm-dd format.")
+        print(" Using default date. start = 2017-08-21")
+        print("start date input: " + st_date_inp)
+        print(" ")
+        st_date_inp   = '2017-08-21'
+        st_date_conv  = dt.datetime.strptime(st_date_inp, '%Y-%m-%d')
+        
+    try:
+        end_date_conv = dt.datetime.strptime(end_date_inp, '%Y-%m-%d')
+    except: 
+        print(" Invalid end date or date format. Please input valid date in yyyy-mm-dd format.")
+        print(" Using default  end date = current date")
+        print("end date input:" + end_date_inp)
+        print(" ")
+        end_date_conv = dt.datetime.today().strftime('%Y-%m-%d')
+
+    
+    print(st_date_conv, end_date_conv)
+     
+    Meas_temp_st = session.query(func.min(Measurement.tobs),func.avg(Measurement.tobs),func.max(Measurement.tobs)).\
+                     filter(Measurement.date >= st_date_conv).\
+                     filter(Measurement.date <= end_date_conv).\
+                     all()
+    return jsonify(Meas_temp_st)
+
+@app.route("/api/v1.0/<start>")
+def tempstartonly(start):
+    
+    st_date_inp  = start
+    
+    try:
+        st_date_conv =  dt.datetime.strptime(st_date_inp, '%Y-%m-%d')
+    
+
+    except:
+        print(" Invalid start date or date format. Please input valid date in yyyy-mm-dd format.")
+        print(" Using default date. start = 2017-08-21")
+        print("start date input: " + st_date_inp)
+        st_date_inp   = '2017-08-21'
+        st_date_conv  = dt.datetime.strptime(st_date_inp, '%Y-%m-%d')
+        
+    print(st_date_conv)
+  
+    Meas_temp_st = session.query(func.min(Measurement.tobs),func.avg(Measurement.tobs),func.max(Measurement.tobs)).\
+                     filter(Measurement.date >= st_date_conv).\
+                     all()
+    return jsonify(Meas_temp_st)
     
 if __name__ == "__main__":
     app.run(debug=True)
